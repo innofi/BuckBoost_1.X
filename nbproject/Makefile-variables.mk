ac9cdaec59d5a921c908dacfd0ae5425c6b885d6
aircable <juergen@kienhoefer.com>
2022-03-25 10:48:59 -0700

    Initial

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# PIC16F15376 configuration
+CND_ARTIFACT_DIR_PIC16F15376=dist/PIC16F15376/production
+CND_ARTIFACT_NAME_PIC16F15376=BuckBoost_1.X.production.hex
+CND_ARTIFACT_PATH_PIC16F15376=dist/PIC16F15376/production/BuckBoost_1.X.production.hex
